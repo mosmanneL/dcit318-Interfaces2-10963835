9343af81b4f04ea32baf938b67b1a687e450689d
mosmanneL <mmadiator05@gmail.com>
2024-07-05 10:10:40 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{// Interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Class that implements IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Another class that implements IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Car and Bicycle
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            // Call the Move method on each instance
+            car.Move();       // Output: Car is moving
+            bicycle.Move();   // Output: Bicycle is moving
+        }
+    }
+}
